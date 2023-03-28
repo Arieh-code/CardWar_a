5f69aa0a96be0b78c012215305ab2bc874add313
Arieh Norton <arieh.norton@gmail.com>
2023-03-28 19:45:55 +0300

    game.cpp

---
+++ sources/game.cpp
@@ -0,0 +1,12 @@
+#include "game.hpp"
+#include <iostream>
+using namespace ariel;
+using namespace std;
+
+Game::Game(Player &p1, Player &p2){};
+void Game::playTurn(){};
+void Game::playAll(){};
+void Game::printLastTurn(){};
+void Game::printLog(){};
+void Game::printStats(){};
+void Game::printWiner(){};
