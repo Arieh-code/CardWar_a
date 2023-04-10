9e049550045a3063f659c979a9855e916e937a1c
Arieh Norton <arieh.norton@gmail.com>
2023-04-10 12:52:58 +0300

    starting to implement the game

--- sources/game.cpp
+++ sources/game.cpp
@@ -1,12 +1,59 @@
 #include "game.hpp"
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
 using namespace ariel;
 using namespace std;
 
-Game::Game(Player &p1, Player &p2){};
-void Game::playTurn(){};
+Game::Game(Player &p1, Player &p2)
+    : player1(p1), player2(p2), roundCounter(0), drawCounter(0)
+{
+    // check if the same player is playing
+    if (&p1 == &p2)
+    {
+        throw invalid_argument("Cannot play game againts yourself");
+    }
+
+    // initialize deck
+    for (int i = 1; i <= 14; i++)
+    {
+        deck.push_back(Card(i, 'H'));
+        deck.push_back(Card(i, 'D'));
+        deck.push_back(Card(i, 'S'));
+        deck.push_back(Card(i, 'C'));
+    }
+
+    shuffleDeck();
+    dealCards();
+}
+
+void Game::shuffleDeck()
+{
+    random_shuffle(deck.begin(), deck.end());
+}
+
+void Game::dealCards()
+{
+    for (int i = 0; i < 26; i++)
+    {
+        player1.addCardToPlayingStack(deck.back());
+        deck.pop_back();
+
+        player2.addCardToPlayingStack(deck.back());
+        deck.pop_back();
+    }
+}
+
+void Game::playTurn()
+{
+    if (player1.stacksize() == 0 || player2.stacksize() == 0)
+
+    {
+        throw runtime_error("Cannot play turn: no cards in playing stack");
+    }
+};
 void Game::playAll(){};
-void Game::printLastTurn(){};
-void Game::printLog(){};
-void Game::printStats(){};
-void Game::printWiner(){};
+void Game::printLastTurn() const {};
+void Game::printLog() const {};
+void Game::printStats() const {};
+void Game::printWiner() const {};
--- sources/game.hpp
+++ sources/game.hpp
@@ -1,18 +1,36 @@
-#pragma once
-#include <iostream>
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <vector>
+#include <string>
 #include "player.hpp"
+#include "card.hpp"
 using namespace std;
+
 namespace ariel
 {
     class Game
     {
+    private:
+        Player player1;
+        Player player2;
+        vector<Card> deck;
+        int roundCounter;
+        int drawCounter;
+
+        void shuffleDeck();
+        void dealCards();
+
     public:
-        Game(Player &player1, Player &player2);
+        Game(Player &p1, Player &p2);
+
         void playTurn();
-        void printLastTurn();
+        void printLastTurn() const;
+        void printLog() const;
+        void printStats() const;
+        void printWiner() const;
         void playAll();
-        void printWiner();
-        void printLog();
-        void printStats();
     };
 }
+
+#endif /* GAME_H_ */
